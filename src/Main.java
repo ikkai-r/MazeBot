a1e21fff9de29747bb96bc3f0fcee5c6436778bf
ikkai-r <rm.sales92@gmail.com>
2023-02-22 20:25:03 +0800

    Added maze.txt and added 2d array for the maze

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Main {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+// Main class
+public class Main {
+
+    // main driver method
+    public static void main(String[] args) throws Exception
+    {
+
+        char [][] arr = new char[0][];
+        int ctr = -1;
+        int size;
+
+        // File path is passed as parameter
+        File file = new File("maze\\maze.txt");
+
+        // Creating an object of BufferedReader class
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        // Declaring a string variable
+        String st;
+
+        // Condition holds true till
+        // there is character in a string
+
+        while ((st = br.readLine()) != null) {
+
+            if(ctr == -1) {
+                size = Integer.parseInt(st);
+                arr = new char[size][size];
+            } else {
+                arr[ctr] = st.toCharArray();
+            }
+            ctr++;
+        }
+
+    }
+
+
 }
